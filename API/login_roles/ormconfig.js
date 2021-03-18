90c9fca7cb74c7fa0a45554621aa403ef149ccbb
Armando <leanluminango@gmail.com>
2021-03-18 14:12:24 -0600

    Intento correccion
    Se intentara realizar el .env

--- API/login_roles/ormconfig.js
+++ API/login_roles/ormconfig.js
@@ -1,10 +1,10 @@
-{
+module.exports = {
     "type": "mysql",
-    "host": "env.DB_HOST",
-    "port": "env.DB_PORT",
-    "username": "env.DB_USERNAME",
-    "password": "env.DB_PASSWORD",
-    "database": "env.DB_NAME",
+    "host": env.DB_HOST,
+    "port": env.DB_PORT,
+    "username": env.DB_USERNAME,
+    "password": env.DB_PASSWORD,
+    "database": env.DB_NAME,
     "synchronize": true,
     "logging": false,
     "entities": [
