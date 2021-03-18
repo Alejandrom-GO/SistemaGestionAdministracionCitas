53e2d3309b74e8ada0e283b0d7963eb7b8a2382b
Armando <leanluminango@gmail.com>
2021-03-18 14:22:03 -0600

    Update ormconfig.js

--- API/login_roles/ormconfig.js
+++ API/login_roles/ormconfig.js
@@ -1,10 +1,10 @@
 module.exports = {
     "type": "mysql",
-    "host": env.DB_HOST,
-    "port": env.DB_PORT,
-    "username": env.DB_USERNAME,
-    "password": env.DB_PASSWORD,
-    "database": env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
     "synchronize": true,
     "logging": false,
     "entities": [
